ec3b7cdeb2b3983b191294eeb650f4a5e2aa50a5
MH-Rohan-JU <rohan70302@gmail.com>
2024-12-25 19:03:42 +0600

    initial commit

--- instructor.h
+++ instructor.h
@@ -1,9 +1,17 @@
 #ifndef INSTRUCTOR_H
 #define INSTRUCTOR_H
 #include "learner.h"
+#include <string>
+using namespace std;
 
 class instructor : public learner {
+  private:
+    string instructorName;
+    int insPassword;
+
   public:
+    void registration();
+    void login();
     void addCourses();
     void showCourses();
     void deleteCourses();
--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include "learner.h"
 #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-#define endl "\n"
-#define ll long long
 
-void MainMenu(learner &l1, learner &l2) {
+void regMenu(learner &l1, learner &l2) {
+
     fstream file;
     file.open("Learners.dat", ios::app | ios::in | ios::out | ios::binary);
     file.seekg(0, ios ::end);
@@ -13,6 +13,72 @@ void MainMenu(learner &l1, learner &l2) {
     ++serial;
     int i, pos;
 
+    cout << "\n1. Register as an Instructor." << endl;
+    cout << "2. Register as a Learner." << endl;
+    cout << "3. Exit." << endl;
+    int option;
+    cout << "Choose an option: ";
+    cin >> option;
+    switch (option) {
+    case 1:
+        cout << "Will be updated later!" << endl;
+        break;
+    case 2:
+        l1.registration(serial);
+        file.write(reinterpret_cast<char *>(&l1), sizeof(l1));
+        cout << "\nRegistration is successful.\nYour ID no. is: " << serial
+             << ".\nPreserve this ID number for future logins.\n"
+             << endl;
+        break;
+    case 3:
+        cout << "\nBest of Luck! Goodbye..." << endl;
+        exit(0);
+        break;
+    }
+    file.close();
+}
+void logMenu(learner &l1, learner &l2) {
+
+    fstream file;
+    file.open("Learners.dat", ios::app | ios::in | ios::out | ios::binary);
+    file.seekg(0, ios ::end);
+    int ts = file.tellg();
+    int serial = ts / sizeof(learner);
+    ++serial;
+    int i, pos;
+
+    cout << "\n1. Login as an Instructor." << endl;
+    cout << "2. Login as a Learner." << endl;
+    cout << "3. Exit." << endl;
+
+    int option;
+    cout << "Choose an option: ";
+    cin >> option;
+
+    switch (option) {
+    case 1:
+        break;
+    case 2:
+        i = l1.login();
+        pos = (i - 1) * sizeof(learner);
+        file.seekg(pos);
+        file.read(reinterpret_cast<char *>(&l2), sizeof(l2));
+        l1 == l2;
+        cout << endl;
+        break;
+
+    case 3:
+        cout << "\nBest of Luck! Goodbye..." << endl;
+        exit(0);
+        break;
+    default:
+        cout << "Invalid Option! Please try again..." << endl;
+        return;
+    }
+    file.close();
+}
+
+void MainMenu(learner &l1, learner &l2) {
     cout << "####################################" << endl;
     cout << "#              HOMEPAGE            #" << endl;
     cout << "####################################" << endl;
@@ -23,23 +89,13 @@ void MainMenu(learner &l1, learner &l2) {
     int option;
     cout << "Choose an option: ";
     cin >> option;
+
     switch (option) {
     case 1:
-        l1.registration(serial);
-        file.write(reinterpret_cast<char *>(&l1), sizeof(l1));
-        cout << "\nRegistration is successful.\nYour ID no. is: " << serial
-             << ".\nPreserve this ID number for future logins.\n"
-             << endl;
+        regMenu(l1, l2);
         break;
     case 2:
-        i = l1.login();
-        pos = (i - 1) * sizeof(learner);
-        file.seekg(pos);
-        file.read(reinterpret_cast<char *>(&l2), sizeof(l2));
-        l1 == l2;
-        // l2.showdata();
-        cout << endl;
-        // l1.showdata();
+        logMenu(l1, l2);
         break;
     case 3:
         cout << "\nUnavailable now!" << endl;
@@ -53,10 +109,12 @@ void MainMenu(learner &l1, learner &l2) {
         break;
     }
 }
+
 int main() {
     learner l1, l2;
     fstream file;
-    file.open("Learners.dat", ios::app | ios::in | ios::out | ios::binary);
+    // file.open("Learners.dat", ios::app | ios::in | ios::out |
+    // ios::binary);
     while (true) {
         MainMenu(l1, l2);
     }
